87487980a8c6ede50b8469119bb883796570a283
Ron Thomas <streiff@ron-thomas-computer.local>
2009-06-30 22:31:43 -0600

    tried to get it to work... but it's getting late and it's due in a few hours.

--- 200906_I_Claudius/src/com/rontologist/solium/romannumerals/common/ejb/NumberLocal.java
+++ 200906_I_Claudius/src/com/rontologist/solium/romannumerals/common/ejb/NumberLocal.java
@@ -2,11 +2,9 @@ package com.rontologist.solium.romannumerals.common.ejb;
 
 import com.rontologist.solium.romannumerals.server.number.RomanNumber;
 
-import javax.ejb.Local;
 import javax.ejb.Remote;
-import java.io.Serializable;
 
 @Remote
-public interface NumberRemote extends Serializable {
+public interface NumberRemote {
     RomanNumber getRomanNumber(String s);
 }
