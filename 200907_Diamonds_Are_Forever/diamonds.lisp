1e965d5f60016d56f335267ad635c6c1221980e9
Ron Thomas <the.ront@gmail.com>
2009-07-03 08:15:59 -0600

    added command-line arguments

--- 200907_Diamonds_Are_Forever/diamonds.lisp
+++ 200907_Diamonds_Are_Forever/diamonds.lisp
@@ -1,23 +1,24 @@
 ; Solium Entry for July 2009. Written in Lisp.
-; Written with the CLISP Lisp interpreter, and uses
-; its extensions for command line arguments
+; Written with the CLISP Lisp interpreter, and uses its way of accessing
+; command line arguments.
+;
+; This is pure lisp -- no variables at all. :D
 
 ; ----------------------------------------------------------------------------
 ; Character manipulation
-(defun prev-char (x) (code-char (- (char-code x) 1)))
-
-(defun next-char (x) (code-char (+ (char-code x) 1)))
+(defun add-char (x y) (code-char (+ (char-code x) y)))
+(defun prev-char (x) (add-char x -1))
+(defun next-char (x) (add-char x 1))
 
 ; ----------------------------------------------------------------------------
 ; Character list generators
+(defun all-chars (x) (reverse (all-chars-rev x)))
 (defun all-chars-rev (x) 
     (if (eql x #\A) 
         (list x) 
         (cons x (all-chars-rev (prev-char x)))
     )
 )
-
-(defun all-chars (x) (reverse (all-chars-rev x)))
 
 (defun whitespace (x)
     (if (eql x 0) 
@@ -27,11 +28,9 @@
 )
     
 (defun outter-space (x y) (- (- (length y) (position x y)) 1))
-
 (defun inner-space (x y)
-    (- (- (* 2 (- (length y) 1)) (* 2(outter-space x y))) 1)
+    (- (* 2 (- (length y) 1)) (* 2(outter-space x y)) 1)
 )
-
 
 ; ----------------------------------------------------------------------------
 ; Diamond formatters
@@ -60,5 +59,21 @@
 )
 
 ; ----------------------------------------------------------------------------
+; Util functions
+(defun usage-error ()
+    (error "~A~%" "Requires one command line argument with a single character.")
+)
+
+(defun command-arg-char ()
+    (if (eql 1 (length *args*))
+        (if (eql 1 (length (car *args*)))
+            (char-upcase (char (car *args*) 0))
+            (usage-error)
+        )
+        (usage-error)
+    )
+)
+
+; ----------------------------------------------------------------------------
 ; Main script
-(format t "~A" (format-diamond #\A (all-chars #\D)))
+(format t "~A" (format-diamond #\A (all-chars (command-arg-char))))
