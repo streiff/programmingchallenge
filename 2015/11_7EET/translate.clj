466b9a8b172c1b4e68c183f22c0785a3f47dade9
Ron Thomas <ron.thomas@solium.com>
2015-11-09 10:12:32 -0700

    tweaks

--- 2015/11_7EET/translate.clj
+++ 2015/11_7EET/translate.clj
@@ -1,72 +1,84 @@
 (import '(java.io BufferedReader))
 
-(def simple-sub-map (array-map 
-    #"(?i)10100111001"      "leet"
-    #"(?i)n00b"             "newbie"
-    #"(?i)pwnd"             "pwned"
-    #"(?i)pwnt"             "pwned"
-    #"(?i)dafuq"            "what the f**k"
-    #"(?i)j00"              "you"
-    #"(?i)joo"              "you"
-    #"(?i)vv"               "w"
-    #"(?i)tomoz"            "tomorrow"
-    #"(?i)(^|\W)teh(\W|$)"  "$1the$2"
-    #"(?i)'d"               "ed"
-    #"(?i)d00d"             "dude"
-    #"(?i)<3"               "love"
-    #"(?i)txt"              "text"
-    #"(?i)(^|\W)2(\W|$)"    "$1to$2"
-    #"(?i)(^|\W)4(\W|$)"    "$1for$2"
-    #"(?i)(^|\W)kk?(\W|$)"  "$1OK$2"
-    #"(?i)(^|\W)y(\W|$)"    "$1why$2"
-    #"(?i)(^|\W)ur(\W|$)"   "$1you're$2"
-    #"(?i)(^|\W)u(\W|$)"    "$1you$2"
-    #"(?i)(\w)8|8(\w)"      "$1ate$2"
-    #"(?i)\$"               "s"
-    #"(?i)\("               "c"
-    #"(?i)z"                "s"
-    #"(?i)5"                "s"
-    #"(?i)@"                "a"
-    #"(?i)4"                "a"
-    #"(?i)3"                "e"
-    #"(?i)7"                "t"
-    #"(?i)\+"               "t"
-    #"(?i)\#"               "h"
-    #"(?i)0"                "o"
-    #"(?i)\\/\\/"           "w"
-    #"(?i)/\\"              "a"
-    #"(?i)\^"               "a"
-    #"(?i)\\/"              "v"
-    #"(?i)8"                "b"
-    #"(?i)\|_\|"            "u"
-    #"(?i)\|-\|"            "h"
-    #"(?i)Я"                "r"
-    #"(?i)\|<"              "k"
-    #"(?i)\[\)"             "d"
-    #"(?i)\|\)"             "d"
-    #"(?i)><"               "x"
-))
-
 (def dict-word-list (clojure.string/split (slurp "/usr/share/dict/words") #"\n"))
 
+(def simple-sub-map (array-map 
+    #"(?i)10100111001"      '("leet")
+    #"(?i)n00b"             '("newbie")
+    #"(?i)pwnd"             '("pwned")
+    #"(?i)pwnt"             '("pwned")
+    #"(?i)dafuq"            '("what the f**k")
+    #"(?i)j00"              '("you")
+    #"(?i)joo"              '("you")
+    #"(?i)vv"               '("w")
+    #"(?i)tomoz"            '("tomorrow")
+    #"(?i)(^|\W)teh(\W|$)"  '("$1the$2")
+    #"(?i)'d"               '("ed")
+    #"(?i)d00d"             '("dude")
+    #"(?i)<3"               '("love")
+    #"(?i)txt"              '("text")
+    #"(?i)(^|\W)2(\W|$)"    '("$1to$2")
+    #"(?i)(^|\W)4(\W|$)"    '("$1for$2")
+    #"(?i)(^|\W)kk?(\W|$)"  '("$1OK$2")
+    #"(?i)(^|\W)y(\W|$)"    '("$1why$2")
+    #"(?i)(^|\W)ur(\W|$)"   '("$1you're$2")
+    #"(?i)(^|\W)u(\W|$)"    '("$1you$2")
+    #"(?i)(\w)8|8(\w)"      '("$1ate$2")
+    #"(?i)\$"               '("s")
+    #"(?i)\("               '("c")
+    #"(?i)z"                '("s")
+    #"(?i)5"                '("s")
+    #"(?i)@"                '("a")
+    #"(?i)4"                '("a")
+    #"(?i)3"                '("e")
+    #"(?i)7"                '("t" "and" "anned" "ant")
+    #"(?i)\+"               '("t")
+    #"(?i)\#"               '("h")
+    #"(?i)0"                '("o")
+    #"(?i)\\/\\/"           '("w")
+    #"(?i)/\\"              '("a")
+    #"(?i)\^"               '("a")
+    #"(?i)\\/"              '("v")
+    #"(?i)8"                '("b")
+    #"(?i)\|_\|"            '("u")
+    #"(?i)\|-\|"            '("h")
+    #"(?i)Я"                '("r")
+    #"(?i)\|<"              '("k")
+    #"(?i)\[\)"             '("d")
+    #"(?i)\|\)"             '("d")
+    #"(?i)><"               '("x")
+    #"(?i)x"                '("ks" "cks")
+    #"(?i)1|!"              '("i" "l")
+    #"(?i)c"                '("see" "c" "sea")
+    #"(?i)b"                '("be" "B" "bee")
+    #"(?i)&"                '("and" "anned" "ant")
+))
+
+
 (defn preprocess-punctuation [lines]
-    (map (fn [line] (clojure.string/replace line #"(\.|,|\?)" " $1 ")) lines))
+    (map #(clojure.string/replace % #"(\.|,|\?)" " $1 ") lines))
 
 (defn split-words [lines]
-    (map (fn [line] (clojure.string/split line #"\s+" )) lines))
+    (map #(clojure.string/split % #"\s+" ) lines))
 
 (defn prepare-list [words]
-    (map (fn [word] (list word)) words))
+    (map list words))
+
+(defn word-regex-replace [word regex replacement]
+    (clojure.string/replace word regex replacement))
+
+(defn word-regex-replace-multiple [word regex replacements]
+    (map #(word-regex-replace word regex %) replacements))
 
 (defn word-simple-sub [word]
-    (distinct (map (fn [key] (clojure.string/replace word key (get simple-sub-map key))) (keys simple-sub-map))))
+    (distinct (flatten (map #(word-regex-replace-multiple word % (get simple-sub-map %)) (keys simple-sub-map)))))
 
 (defn process-word-simple-sub [words num-passes]
     (if (= num-passes 0) words
-        (process-word-simple-sub (distinct (flatten (map word-simple-sub words))) (- num-passes 1))))
+        (recur (distinct (flatten (map word-simple-sub words))) (- num-passes 1))))
 
 (defn simple-subs [word-lists]
-    (map (fn [words] (process-word-simple-sub words (count (first words)))) word-lists))
+    (map #(process-word-simple-sub % (count (first %))) word-lists))
 
 (defn remove-accents [word-lists]
     (map (fn [words]
@@ -82,9 +94,22 @@
               filtered-words (filter some? (map (fn [word] (some (fn [dict-word] (if (.equalsIgnoreCase word dict-word) dict-word)) dict-word-list)) words))]
             (list (if (> (count filtered-words) 0) (first filtered-words) first-word)))) word-lists))
 
+(defn sentencfy [word-lists]
+    (clojure.string/split
+        (clojure.string/replace 
+            (clojure.string/join " " (flatten word-lists))
+            #"\s*\." ".")
+        #"\. "))
 
-(println
-         (->> (line-seq (BufferedReader. *in*))
+(defn wordify [word-lists]
+    (apply str (interpose ". " 
+        (map #(str (.toUpperCase (subs % 0 1)) (subs % 1)) (sentencfy word-lists)))))
+
+(defn exclamationify [s]
+    (clojure.string/replace (clojure.string/replace s #"(?i)!1|1!|!one|one!", "!!")
+                            #"(?i)!eleven|eleven!" "!!!"))
+
+(println (->> (line-seq (BufferedReader. *in*))
               (preprocess-punctuation)
               (split-words)
               (flatten)
@@ -92,12 +117,5 @@
               (simple-subs)
               (filter-choices)
               (remove-accents)
-))
-
-; not covered by simple
-; x,X    -> -ks-,-cks-
-; 1      -> i,I,l,L
-; !      -> i,I,!
-; c,C    -> see,C,sea
-; b,B    -> be,B,bee
-; &,7    -> and,anned,ant (may be used in the middle of a word)
+              (wordify)
+              (exclamationify)))
