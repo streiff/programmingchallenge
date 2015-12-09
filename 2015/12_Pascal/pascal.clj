04a1d12dee710b309784995f6c7f2ab120727540
Ron Thomas <ron.thomas@solium.com>
2015-12-09 09:22:09 -0700

    tweaks

--- 2015/12_Pascal/pascal.clj
+++ 2015/12_Pascal/pascal.clj
@@ -1,48 +1,43 @@
 (defn calculate-next-row [previous-row]
-    (map #(+ (bigint %1) (bigint %2)) (cons 0 previous-row) (concat previous-row '(0))))
+  (if (= previous-row nil)
+    '[1]
+    (map #(+ (bigint %1) (bigint %2)) (cons 0 previous-row) (concat previous-row '(0)))))
 
-(defn append-rows [rows number-of-rows]
+(defn calculate-pascal
+  ([number-of-rows] (calculate-pascal number-of-rows '[]))
+  ([number-of-rows rows]
     (if (== number-of-rows 0)
-        rows
-        (recur (concat rows (vector (calculate-next-row (last rows)))) 
-               (- number-of-rows 1))))
-
-(defn calculate-pascal [number-of-rows]
-    (cond (<= number-of-rows 0) []
-          (== number-of-rows 1) '[[1]]
-          :else (append-rows '[[1]] (- number-of-rows 1))))
+      rows
+      (recur (dec number-of-rows) (concat rows (vector (calculate-next-row (last rows))))))))
 
 (defn xbm-export-line [pascal-row padding-str stilted bits]
-    (let [raw-bytes (clojure.string/join "" (concat
-                         padding-str
-                         (map #(if (= (mod % 2) 1) bits "00") pascal-row)
-                         padding-str))
-          aligned-bytes (if stilted (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
-                                    raw-bytes)]
-        (str aligned-bytes "\n" aligned-bytes)))
+  (let [raw-bytes (clojure.string/join "" (concat
+                    padding-str
+                    (map #(if (mod % 2) bits "00") pascal-row)
+                    padding-str))
+        aligned-bytes (if stilted 
+                        (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
+                        raw-bytes)]
+    (str aligned-bytes "\n" aligned-bytes)))
 
 (defn xbm-export-row [pascal-row pascal-rows-size]
-    (def padding (* 4 (- pascal-rows-size (count pascal-row))))
-    (def padding-str (repeat (/ padding 4) "0"))
-    (def stilted (== (mod padding 8) 4))
-    (def bits (if stilted '("81" "C3" "E7" "FF")
-                          '("18" "3C" "7E" "FF")))
-
-    (def all-bytes (clojure.string/join "\n" (map #(xbm-export-line pascal-row padding-str stilted %) bits)))
-
-    (clojure.string/replace all-bytes #"(..)" "0x$1, "))
+  (let [padding (* 4 (- pascal-rows-size (count pascal-row)))
+        padding-str (repeat (/ padding 4) "0")
+        stilted (== (mod padding 8) 4)
+        bits (if stilted 
+                '("81" "C3" "E7" "FF")
+                '("18" "3C" "7E" "FF"))
+        all-bytes (clojure.string/join "\n" (map #(xbm-export-line pascal-row padding-str stilted %) bits))]
+    (clojure.string/replace all-bytes #"(..)" "0x$1, ")))
 
 (defn xbm-export [pascal-rows]
-    (def pascal-rows-size (count pascal-rows))
-
+  (let [pascal-rows-size (count pascal-rows)]
     (str "#define test_width  " (* pascal-rows-size 8) "\n"
          "#define test_height " (* pascal-rows-size 8) "\n"
          "static char test_bits[] = {\n"
          (clojure.string/replace 
              (clojure.string/join "\n" (map #(xbm-export-row % pascal-rows-size) pascal-rows))
              #", $", "")
-         "};\n"))
+         "};\n")))
 
-(let [pascal (calculate-pascal (Integer/valueOf (first *command-line-args*)))]
-  (println (xbm-export pascal))
-)
+(println (xbm-export (calculate-pascal(Integer/valueOf (first *command-line-args*)))))
