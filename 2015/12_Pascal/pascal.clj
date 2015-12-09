48682a653bf09ee4bc2053554177110eb0f8c5da
Ron Thomas <ron.thomas@solium.com>
2015-12-09 10:19:56 -0700

    small tweaks

--- 2015/12_Pascal/pascal.clj
+++ 2015/12_Pascal/pascal.clj
@@ -13,20 +13,19 @@
 (defn xbm-export-line [pascal-row padding-str stilted bits]
   (let [raw-bytes (clojure.string/join "" (concat
                     padding-str
-                    (map #(if (mod % 2) bits "00") pascal-row)
-                    padding-str))
-        aligned-bytes (if stilted 
-                        (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
-                        raw-bytes)]
-    (str aligned-bytes "\n" aligned-bytes)))
+                    (map #(if (odd? %) bits "00") pascal-row)
+                    padding-str))]
+        (if stilted 
+            (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
+            raw-bytes)))
 
 (defn xbm-export-row [pascal-row pascal-rows-size]
   (let [padding (* 4 (- pascal-rows-size (count pascal-row)))
         padding-str (repeat (/ padding 4) "0")
         stilted (== (mod padding 8) 4)
         bits (if stilted 
-                '("81" "C3" "E7" "FF")
-                '("18" "3C" "7E" "FF"))
+                '("81" "81" "C3" "C3" "E7" "E7" "FF" "FF")
+                '("18" "18" "3C" "3C" "7E" "7E" "FF" "FF"))
         all-bytes (clojure.string/join "\n" (map #(xbm-export-line pascal-row padding-str stilted %) bits))]
     (clojure.string/replace all-bytes #"(..)" "0x$1, ")))
 
