ffd09eec35d26637db935fcddc26b6bb97267ade
Ron Thomas <ron.thomas@solium.com>
2015-12-11 09:06:16 -0700

    tweaks

--- 2015/12_Pascal/pascal.clj
+++ 2015/12_Pascal/pascal.clj
@@ -1,27 +1,27 @@
-(defn calculate-next-row [previous-row]
+(defn calc-next-row [previous-row]
   (if previous-row
     (map #(+ (bigint %1) (bigint %2)) (cons 0 previous-row) (concat previous-row '(0)))
     '[1]))
 
-(defn calculate-pascal
-  ([number-of-rows] (calculate-pascal number-of-rows '[]))
+(defn calc-pascal
+  ([number-of-rows] (calc-pascal number-of-rows '[]))
   ([number-of-rows rows]
     (if (== number-of-rows 0)
       rows
-      (recur (dec number-of-rows) (concat rows (vector (calculate-next-row (last rows))))))))
+      (recur (dec number-of-rows) (concat rows (vector (calc-next-row (last rows))))))))
 
 (defn xbm-export-line [pascal-row padding-str stilted bits]
   (let [raw-bytes (str padding-str
                        (clojure.string/join (map #(if (odd? %) bits "00") pascal-row))
                        padding-str)]
-        (if stilted 
-            (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
-            raw-bytes)))
+    (if stilted 
+      (clojure.string/replace raw-bytes #"(.)(.)" "$2$1") 
+      raw-bytes)))
 
 (defn xbm-export-row [pascal-row pascal-rows-size]
-  (let [padding (* 4 (- pascal-rows-size (count pascal-row)))
-        padding-str (apply str (repeat (/ padding 4) "0"))
-        stilted (== (mod padding 8) 4)
+  (let [padding (- pascal-rows-size (count pascal-row))
+        padding-str (apply str (repeat padding "0"))
+        stilted (== (mod padding 2) 1)
         bits (if stilted 
                 '("81" "81" "C3" "C3" "E7" "E7" "FF" "FF")
                 '("18" "18" "3C" "3C" "7E" "7E" "FF" "FF"))
@@ -38,4 +38,4 @@
              #", $", "")
          "};\n")))
 
-(println (xbm-export (calculate-pascal(Integer/valueOf (first *command-line-args*)))))
+(println (xbm-export (calc-pascal(Integer/valueOf (first *command-line-args*)))))
